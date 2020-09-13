0ca7aa3bcfed64f4f1efd9ff4e6c0e15c36c9a18
noa <noabennathan@gmail.com>
2020-09-13 14:17:32 +0300

    myarray

--- my_assert.c
+++ my_assert.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-assert_int(int source, int dest);
+void assert_int(int source, int dest);
 
-assert_int(int source, int dest){
+void assert_int(int source, int dest){
 	if (source != dest){
 		exit(0);
 	}
