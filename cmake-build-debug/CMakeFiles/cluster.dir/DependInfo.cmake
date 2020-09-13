439ed6558c754e40ca281a024d94435f1409ed75
yuvaldoron <XqVaemN@3i%p4P>
2020-09-13 13:33:26 +0300

    Fixed something

---
+++ cmake-build-debug/CMakeFiles/cluster.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/yuval_000/CLionProjects/software_project/algorithm2.c" "C:/Users/yuval_000/CLionProjects/software_project/cmake-build-debug/CMakeFiles/cluster.dir/algorithm2.c.obj"
+  "C:/Users/yuval_000/CLionProjects/software_project/algorithm4.c" "C:/Users/yuval_000/CLionProjects/software_project/cmake-build-debug/CMakeFiles/cluster.dir/algorithm4.c.obj"
+  "C:/Users/yuval_000/CLionProjects/software_project/cluster.c" "C:/Users/yuval_000/CLionProjects/software_project/cmake-build-debug/CMakeFiles/cluster.dir/cluster.c.obj"
+  "C:/Users/yuval_000/CLionProjects/software_project/graph.c" "C:/Users/yuval_000/CLionProjects/software_project/cmake-build-debug/CMakeFiles/cluster.dir/graph.c.obj"
+  "C:/Users/yuval_000/CLionProjects/software_project/linked_list.c" "C:/Users/yuval_000/CLionProjects/software_project/cmake-build-debug/CMakeFiles/cluster.dir/linked_list.c.obj"
+  "C:/Users/yuval_000/CLionProjects/software_project/my_array.c" "C:/Users/yuval_000/CLionProjects/software_project/cmake-build-debug/CMakeFiles/cluster.dir/my_array.c.obj"
+  "C:/Users/yuval_000/CLionProjects/software_project/my_assert.c" "C:/Users/yuval_000/CLionProjects/software_project/cmake-build-debug/CMakeFiles/cluster.dir/my_assert.c.obj"
+  "C:/Users/yuval_000/CLionProjects/software_project/spmat.c" "C:/Users/yuval_000/CLionProjects/software_project/cmake-build-debug/CMakeFiles/cluster.dir/spmat.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cluster.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cluster.dir/algorithm2.c.obj"
+  "CMakeFiles/cluster.dir/algorithm4.c.obj"
+  "CMakeFiles/cluster.dir/cluster.c.obj"
+  "CMakeFiles/cluster.dir/graph.c.obj"
+  "CMakeFiles/cluster.dir/linked_list.c.obj"
+  "CMakeFiles/cluster.dir/my_array.c.obj"
+  "CMakeFiles/cluster.dir/my_assert.c.obj"
+  "CMakeFiles/cluster.dir/spmat.c.obj"
+  "cluster.exe"
+  "cluster.exe.manifest"
+  "cluster.pdb"
+  "libcluster.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cluster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
